7eea603beeb8fec96d95618b3c6164d23d961671
Lhaztle24 <133990469+Lhaztle24@users.noreply.github.com>
2024-04-22 17:14:33 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,7 +1,1 @@
-const checkbox = document.getElementById('check');
-const menu = document.querySelector('.menu');
-
-checkbox.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
 
