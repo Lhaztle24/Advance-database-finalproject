6d179c5f4e576480f06403fb2c2d793608da1b79
Lhaztle24 <133990469+Lhaztle24@users.noreply.github.com>
2024-04-24 19:14:53 +0800

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,12 @@
+<?php 
+$servername = "localhost";
+$username =  "root";
+$password = " ";
+$dbname = "registration";
+
+$conn = new mysqli ("localhost", "root", " ", "root");
+
+if ($conn connection_error) {
+  die ("Connection failed: " . $conn connect_error);
+}
+?>
