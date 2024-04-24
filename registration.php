ddad9703d8772708fda9f0c5fa3c111c978ea7bf
Lhaztle24 <133990469+Lhaztle24@users.noreply.github.com>
2024-04-24 18:52:49 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -8,11 +8,12 @@ if ($_SERVER ["REQUEST_METHOD] = "POST) {
  $Email = $_POST ['email'];
  $Destination = $_POST ['destination'];
  $date = $_POST ['date'];
+ $user_id = $_POST ['user_id'];
 
 
- $stmt = $conn prepare ("INSERT INTO register (Full_name, Email, Destination, date) VALUES (?, ?, ?, ?)
+ $stmt = $conn prepare ("INSERT INTO register (Full_name, Email, Destination, date, user_id) VALUES (?, ?, ?, ?, ?)
 
- $stmt bind_param("ssssi", $Full_name, $Email, $Destination, $Date);
+ $stmt bind_param("ssssi", $Full_name, $Email, $Destination, $Date, $user_id);
 
  if ($stmt excute()) {
   echo "order placed successfully";
